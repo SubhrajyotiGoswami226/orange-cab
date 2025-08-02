74bf5a97021af506ab9810286790bcba382c7f39
SubhrajyotiGoswami226 <75923850+SubhrajyotiGoswami226@users.noreply.github.com>
2025-08-02 15:12:24 +0530

    Update ServiceCard.tsx

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -38,7 +38,7 @@ export const ServiceCard = ({
       navigate('/urbania-booking');
     } else {
       const message = `Hi Team, I am interested for ${name}`;
-      const whatsappUrl = `https://wa.link/bg5sy0?text=${encodeURIComponent(message)}`;
+      const whatsappUrl = `https://wa.me/919394939500?text=${encodeURIComponent(message)}`;
       window.open(whatsappUrl, '_blank');
     }
   };
